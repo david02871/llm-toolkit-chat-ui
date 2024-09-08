3304d023a473e25b5f951052e2c9db8e616b82c6
David Young <david.young@brightminded.com>
2024-09-08 13:15:46 +0100

    Commit current changes

--- src/app/api/run-scripts/route.ts
+++ src/app/api/run-scripts/route.ts
@@ -1,7 +1,10 @@
 import { NextRequest, NextResponse } from "next/server"
+import civicrmAssistant from "@/app/assistants/civicrm-assistant"
 
 export const runtime = "nodejs"
 
 export async function GET(req: NextRequest, res: NextResponse) {
+  let result = await civicrmAssistant.createCiviCrmAssistant()
+  console.log(result)
   return NextResponse.json({}, { status: 200 })
 }
