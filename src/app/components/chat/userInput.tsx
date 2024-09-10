7228fade8a4d9da548fea87b40394779caf7cc86
David Young <david.young@brightminded.com>
2024-09-10 22:03:49 +0100

    Remove bad SVG

--- src/app/components/chat/userInput.tsx
+++ src/app/components/chat/userInput.tsx
@@ -41,23 +41,6 @@ const UserInput = ({
         onSubmit={handleSubmit}
         className="flex items-center bg-background-surface p-2 rounded-3xl max-w-[700px] w-full"
       >
-        <button className="bg-transparent text-text-primary p-2">
-          {/* Attach button, placeholder for icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M15.172 7l-6.586 6.586a1 1 0 01-1.414 0L2 8.828V21h18V3H8.828l4.243 4.243a1 1 0 010 1.414L7 15.172M15.172 7L7 15.172"
-            />
-          </svg>
-        </button>
         <textarea
           ref={textareaRef}
           value={userInput}
